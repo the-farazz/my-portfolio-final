103b14e56777f1d95c14ef6b3ed3a0d36be297e8
Faraz Alam <farazalam706@gmail.com>
2025-06-28 11:43:08 -0700

    12th commit

---
+++ client/next.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  images:{
+    unotimized:true,    
+  }
+}
+ 
+module.exports = nextConfig
